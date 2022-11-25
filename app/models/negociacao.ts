d78fbc9434239aaf79d9d09bb77de21e5c560a8c
Celio Costa <celiorscosta@hotmail.com>
2022-11-25 16:29:59 -0300

    Final

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -5,13 +5,21 @@ export class Negociacao {
         public readonly valor: number
     ) { }
 
-  
+
     get volume(): number {
         return this.quantidade * this.valor;
     }
 
-    get data(): Date{
+    get data(): Date {
         const data = new Date(this._data.getTime());
         return data;
+    }
+
+    public static criaDe(dataString: string, quantidadeString: string, valorString: string): Negociacao {
+        const exp = /-/g;
+        const date = new Date(dataString.replace(exp, ','));
+        const quantidade = parseInt(quantidadeString);
+        const valor = parseFloat(valorString);
+        return new Negociacao(date, quantidade, valor);
     }
 }
